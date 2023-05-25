9ef98523189dffce1afcee16c97bbfed22ec3d83
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-05-25 12:04:32 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import {  useEffect }  from  'react';
+import{
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+   
+}
+from 'react-router-dom';
+
+import Home from './scenes/home/Home'
+
+const ScrollToTop= ()=>{
+  const  {pathname} = useLocation();
+
+  useEffect(()=> {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+
+}
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+      <ScrollToTop/>
+      <Routes>
+        <Route path='/' elemen= {<Home />} />
+
+      </Routes>
+      </BrowserRouter>
+       
+    </div> 
+  );
+}
+
+export default App;
