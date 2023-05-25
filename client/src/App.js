7bad4cd94e684f2ccd0147ca8c7eb710477b4121
Nicholaschibueze <nicholaschibuezemichael@gmail.com>
2023-05-26 00:53:47 +0100

    worked on Global folder and Home Folder

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,12 @@ import{
 from 'react-router-dom';
 
 import Home from './scenes/home/Home'
+import ItemDetails from './itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/confirmation';
+import Navbar from './scenes/global/Navbar';
+import CartMenu from './scenes/global/CartMenu';
+import Footer from './scenes/global/Footer';
 
 const ScrollToTop= ()=>{
   const  {pathname} = useLocation();
@@ -25,11 +31,21 @@ function App() {
   return (
     <div className="app">
       <BrowserRouter>
+      <Navbar />
       <ScrollToTop/>
       <Routes>
-        <Route path='/' elemen= {<Home />} />
+        <Route path='/' element= {<Home />} />
+        <Route path='item/itemId' element= {<ItemDetails />} />
+        <Route path='checkout' element= {<Checkout />} />
+        <Route path='checkout/success' element= {<Confirmation />} />
+
+
+
 
       </Routes>
+      <CartMenu/>
+      <Footer/>
+      
       </BrowserRouter>
        
     </div> 
